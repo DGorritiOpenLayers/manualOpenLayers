63cee9086101d7712386ba290b9bf2e7d555af7e
Gorriti13 <gorriti_13@hotmail.com>
2015-05-13 12:48:59 +0200

    selections on WFS Layers

--- js/10_WFS.js
+++ js/10_WFS.js
@@ -1,5 +1,7 @@
 var map, renderer;
 var styleHydrography, styleBounds, styleCities;
+var base,statesWMS,statesWFS,tasmaniaWaterBodies,tasmaniaCities,tasmaniaStateBoundaries,lakes;
+var selectControl;
 
 function init(){
 //OpenLayers.ProxyHost = "proxy.cgi?url=";
@@ -12,34 +14,37 @@ function init(){
 
     createMap();
 
+    defineSelectControl();
+
+    map.addControl(selectControl);
+
+    selectControl.activate();
+
 }
 
 function createMap(){
 
-    map = new OpenLayers.Map({
-        div: "map",
-        layers: [
-        new OpenLayers.Layer.WMS("OpenLayers WMS",
-            "http://www.ign.es/wms-inspire/ign-base?",
-            {layers: "IGNBaseTodo"} 
-            ),
-        new OpenLayers.Layer.WMS("States WMS",
-            "http://demo.boundlessgeo.com/geoserver/wms",
-            {layers: "topp:states", format: "image/png", transparent: true},
-            {maxScale: 15000000}
-            ),
-        new OpenLayers.Layer.Vector("States", {
-            styleMap: styleBounds,
-            minScale: 15000000,
-            strategies: [new OpenLayers.Strategy.BBOX()],
-            protocol: new OpenLayers.Protocol.WFS({
-                url: "http://demo.boundlessgeo.com/geoserver/wfs",
-                featureType: "states",
-                featureNS: "http://www.openplans.org/topp"
-            }),
-            renderers: renderer
+    base = new OpenLayers.Layer.WMS("OpenLayers WMS",
+        "http://www.ign.es/wms-inspire/ign-base?",
+        {layers: "IGNBaseTodo"} 
+        );
+    statesWMS = new OpenLayers.Layer.WMS("States WMS",
+        "http://demo.boundlessgeo.com/geoserver/wms",
+        {layers: "topp:states", format: "image/png", transparent: true},
+        {maxScale: 15000000}
+        );
+    statesWFS = new OpenLayers.Layer.Vector("States", {
+        styleMap: styleBounds,
+        minScale: 15000000,
+        strategies: [new OpenLayers.Strategy.BBOX()],
+        protocol: new OpenLayers.Protocol.WFS({
+            url: "http://demo.boundlessgeo.com/geoserver/wfs",
+            featureType: "states",
+            featureNS: "http://www.openplans.org/topp"
         }),
-        new OpenLayers.Layer.Vector("Tasmania Water Bodies", {
+        renderers: renderer
+    });
+    tasmaniaWaterBodies = new OpenLayers.Layer.Vector("Tasmania Water Bodies", {
             //minScale: 15000000,
             styleMap: styleHydrography,
             strategies: [new OpenLayers.Strategy.BBOX()],
@@ -49,8 +54,8 @@ function createMap(){
                 featureNS: "http://www.openplans.org/topp"
             }),
             renderers: renderer
-        }),
-        new OpenLayers.Layer.Vector("Tasmania Cities", {
+        });
+    tasmaniaCities = new OpenLayers.Layer.Vector("Tasmania Cities", {
             //minScale: 15000000,
             styleMap: styleCities,
             strategies: [new OpenLayers.Strategy.BBOX()],
@@ -60,8 +65,8 @@ function createMap(){
                 featureNS: "http://www.openplans.org/topp"
             }),
             renderers: renderer
-        }),
-        new OpenLayers.Layer.Vector("Tasmania State Boundaries", {
+        });
+    tasmaniaStateBoundaries = new OpenLayers.Layer.Vector("Tasmania State Boundaries", {
             //minScale: 15000000,
             styleMap: styleBounds,
             strategies: [new OpenLayers.Strategy.BBOX()],
@@ -71,8 +76,8 @@ function createMap(){
                 featureNS: "http://www.openplans.org/topp"
             }),
             renderers: renderer
-        }),
-        new OpenLayers.Layer.Vector("Lakes", {
+        });
+    lakes = new OpenLayers.Layer.Vector("Lakes", {
             //minScale: 15000000,
             styleMap: styleHydrography,
             strategies: [new OpenLayers.Strategy.BBOX()],
@@ -82,8 +87,11 @@ function createMap(){
                 featureNS: "http://boundlessgeo.com"
             }),
             renderers: renderer
-        })
-        ],
+        });
+
+    map = new OpenLayers.Map({
+        div: "map",
+        layers: [base,statesWMS,statesWFS,tasmaniaWaterBodies,tasmaniaCities,tasmaniaStateBoundaries,lakes],
         controls:[
         new OpenLayers.Control.Attribution(),
         new OpenLayers.Control.PanZoom(),
@@ -97,24 +105,47 @@ function createMap(){
 
 }
 
+function defineSelectControl(){
+
+    selectControl = new OpenLayers.Control.SelectFeature(
+        [statesWFS,tasmaniaWaterBodies,tasmaniaCities,tasmaniaStateBoundaries,lakes],
+        {
+            clickout: true, toggle: false,
+            multiple: false, hover: false,
+                    toggleKey: "ctrlKey", // ctrl key removes from selection
+                    multipleKey: "shiftKey" // shift key adds to selection
+                }
+                );
+
+}
+
 function defineStyles(){
 
-    styleHydrography = new OpenLayers.StyleMap({
-        fillColor: '#0055ee',
+    var selectStyle = new OpenLayers.Style({
+        fillColor: 'blue',
         fillOpacity: '0.4',
-        strokeColor: '#0055ee'
+        strokeColor: 'blue'
+    });
+
+    styleHydrography = new OpenLayers.StyleMap({
+        'default': {fillColor: '#0055ee',
+        fillOpacity: '0.4',
+        strokeColor: '#0055ee'},
+        'select': selectStyle
     });
 
     styleBounds = new OpenLayers.StyleMap({
-        fillColor: 'red',
+        'default': {fillColor: 'red',
         fillOpacity: '0.4',
-        strokeColor: 'red'
+        strokeColor: 'red'},
+        'select': selectStyle
     });
 
     styleCities = new OpenLayers.StyleMap({
-        fillColor: 'black',
+        'default': {fillColor: 'black',
         strokeColor: 'black',
-        pointRadius: '5'
+        pointRadius: '5'},
+        'select': selectStyle
     });
 
 
